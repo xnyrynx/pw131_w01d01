80a5fd4c2889fe41b73f3a424b03c10386413072
Rynxes Melgar <rynxesmelgar@Rynxess-MBP.lan>
2023-08-22 18:31:13 -0700

    gitnoted

---
+++ day1/cw_problem3.py
@@ -0,0 +1,6 @@
+def multiply(answer):
+    num1, num2 = 1,2
+    answer = num1 * num2
+    return answer
+
+    print(answer)
--- day1/python_loop.py
+++ day1/python_loop.py
@@ -1,3 +1,6 @@
 daily = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 for days in daily:
     print(days.title())
+
+today.days[2]
+print(today)
